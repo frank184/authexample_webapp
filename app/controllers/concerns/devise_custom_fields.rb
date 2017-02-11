71d9a56df18e4d2f1b1e8d1d3b34fd50007d0aac
Frank <fbelanger@ackroo.com>
2017-02-11 16:26:23 -0500

    basic Devise setup & Devise::TokenAuthenticatable

---
+++ app/controllers/concerns/devise_custom_fields.rb
@@ -0,0 +1,13 @@
+module DeviseCustomFields
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :token_authenticatable, :registerable,
+       :recoverable, :rememberable, :trackable, :validatable
+
+  before_save :ensure_authentication_token
+end
