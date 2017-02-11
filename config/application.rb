04830bb9a8a8f84e9b3a90bbaea16c51253284c2
Frank <fbelanger@ackroo.com>
2017-02-11 16:10:18 -0500

    DB, RSpec & testing framework helper setup

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,18 @@ module AuthexampleWebapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.view_specs false
+      g.helper_specs false
+      g.stylesheets = false
+      g.javascripts = false
+      g.helper = false
+    end
   end
 end
