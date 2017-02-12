e002fe30fb20031e278c55bffbd0eea85d74b29e
Frank <fbelanger@ackroo.com>
2017-02-11 22:02:42 -0500

    removing constraint for heroku without a DNS record

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+  namespace :api, defaults: { format: :json } do
     scope :v1 do
       mount V1::Api => '/'
     end
